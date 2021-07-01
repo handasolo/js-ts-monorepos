f4f53e19556d99a38924cf144a049b278c64235e
Nikhil Handa <nikhil.handa@kuliza.com>
2021-07-01 18:56:34 +0530

    chore(utils): lets end this

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -21,6 +21,6 @@
  *
  * @packageDocumentation
  */
-
+///aaaaaaa
 export { isChannel, isMessage, isTeam, isTypedArray } from "./type-guards";
 export { IChannel, IMessage, ITeam, IUser } from "./types";
